9e517f7d92044566dff02b8a8f4df165965fe650
Steven Tey <stevensteel97@gmail.com>
2024-02-17 20:53:30 -0800

    Update action.ts

--- app/action.ts
+++ app/action.ts
@@ -8,8 +8,8 @@ export async function verifyPassword(_prevState: any, data: FormData) {
   // artificial delay
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  // randomly throw an error (10 percent chance)
-  if (Math.random() < 0.1) {
+  // randomly throw an error (1 percent chance)
+  if (Math.random() < 0.01) {
     return { error: "Invalid password" };
   }
 
